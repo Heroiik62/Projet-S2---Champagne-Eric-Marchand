5e429cf2d11513933aa385f523e4c96d08fe0c43
Lucas Bertrand <lucas.bertrand@etudiant.univ-reims.fr>
2018-06-12 15:10:08 +0200

    ajout attributs et accesseur sur idPers

---
+++ personne.class.php
@@ -0,0 +1,21 @@
+<?php
+
+class Personne {
+    
+    private $idPers = null;
+    private $nomPers = null;
+    private $pnomPers = null;
+    private $dnaisPers = null;
+    private $mailPers = null;
+    private $adressePers = null;
+    private $cpPers = null;
+    private $villePers = null;
+    private $telPers = null;
+    
+    public function getId() {
+        return $this->idPers;
+    }
+    
+    
+    
+}
