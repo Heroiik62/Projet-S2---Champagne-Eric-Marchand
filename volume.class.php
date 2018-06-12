ac84e717275b7f5439a6061022c04e3655f7354a
Lucas Bertrand <lucas.bertrand@etudiant.univ-reims.fr>
2018-06-12 15:36:44 +0200

    attributs + deux accesseurs sur id et volume

---
+++ volume.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class Volume {
+    
+    private $idVolume = null;
+    private $volume = null;
+    
+    public function getId() {
+        return $this->idVolume;
+    }    
+    
+    public function getVolume($id) {
+        
+    }
+    
+}
