429ae78ec3ea2884e2ba78857778e5ab953f768a
Lucas Bertrand <lucas.bertrand@etudiant.univ-reims.fr>
2018-06-12 15:50:39 +0200

    methode getVolume qui renvoie le volume correspondant à l'id en param

--- volume.class.php
+++ volume.class.php
@@ -10,7 +10,17 @@ class Volume {
     }    
     
     public function getVolume($id) {
-        
+        $volume = myPDO::getInstance()->prepare('SELECT * 
+                                                FROM volume 
+                                                WHERE idVolume = :id');
+        $volume->execute(array(':id' => $id));
+        $volume->setFetchMode(PDO::FETCH_CLASS, 'Volume');
+        if (($reponse = volume->fetch()) !== false) {
+            return $reponse;
+        }
+        else {
+        throw new Exception('Volume introuvable');
+        }
     }
     
 }
