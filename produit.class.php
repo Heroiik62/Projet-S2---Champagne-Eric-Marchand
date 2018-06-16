60b9ff46374f63072c6d4835ae23ff5fe2170ba6
Darkweak <sylvain.combraque@etudiant.univ-reims.fr>
2018-06-16 20:12:24 +0200

    Fix produit class

--- produit.class.php
+++ produit.class.php
@@ -21,63 +21,51 @@ class Produit {
 	}
 
 	public static function createFromID($idProduit) {
-		$produit = myPDO::getInstance()->prepare('SELECT * FROM produit WHERE idProduit = :idProduit');
-		$produit->execute(array(':idProduit' => $idProduit));
-		$produit->setFetchMode(PDO::FETCH_CLASS, "Produit");
-		if(($p = $produit->fetch()) !== FALSE) {	
-			return $p;
-		}
-		else {
-			throw new Exception('Produit introuvable');
-		}
+        $request =<<<SQL
+SELECT * FROM produit ORDER BY idProduit WHERE idProduit = ?
+SQL;
+        $produit = myPDO::getInstance()->prepare($request);
+        $produit->setFetchMode(PDO::FETCH_CLASS, __CLASS__);
+        $produit->execute([$idProduit]);
+
+        if ($produit)
+            return $produit->fetchAll();
+        else
+            throw new Exception('Erreur, aucun produit trouvé');
 	}
 
 	public static function getProduits() {
-		$images = myPDO::getInstance()->prepare('SELECT * FROM produit ORDER BY idProduit');
-		$images->execute();
-		$images->setFetchMode(PDO::FETCH_CLASS, "Produit");
-		$img = null;
-        $img = <<<HTML
-        <div class="container col-md-8"><nav class="navbar navbar-expand-lg navbar-dark bg-dark row"><ul>
-HTML;
-		while($i = $images->fetch()) {
-            $href = strtolower($i->getNom());
-            $href = preg_replace("/ /", "-", $href);
-			$img .= "<li id='produits'><a href ='#{$href}'>{$i->getNom()}</a></li>";
-		}
-        $img .= "</ul></nav></div>";
-		return $img;
+        $request =<<<SQL
+SELECT * FROM produit ORDER BY idProduit
+SQL;
+        $produit = myPDO::getInstance()->prepare($request);
+        $produit->setFetchMode(PDO::FETCH_CLASS, __CLASS__);
+        $produit->execute([]);
+
+        if ($produit)
+            return $produit->fetchAll();
+        else
+            throw new Exception('Erreur, aucun produit trouvé');
+//		while($i = $images->fetch()) {
+//            $href = strtolower($i->getNom());
+//            $href = preg_replace("/ /", "-", $href);
+//			$img .= "<li id='produits'><a href ='#{$href}'>{$i->getNom()}</a></li>";
+//		}
+//        $img .= "</ul></nav></div>";
 	}
     
     public static function getProduitsAvecGamme() {
-		$produit = myPDO::getInstance()->prepare('SELECT * FROM produit ORDER BY idProduit');
-		$produit->execute();
-		$produit->setFetchMode(PDO::FETCH_CLASS, "Produit");
-		$rep = null;
-        $rep = <<<HTML
-        <hr>
-        <div class="container">        
-HTML;
-		while($p = $produit->fetch()) {
-            $href = strtolower($p->getNom());
-            $href = preg_replace("/ /", "-", $href);
-            $rep .= <<<HTML
-            <h1 class="display-4 nomProd" id='{$href}'>{$p->getNom()}</h1>
-HTML;
-            $test = Gamme::getContenances($p->getId());
-            $size = sizeof($test);
-            for($i = 0; $i < $size; $i++) {
-            	$rep .= "<div class='imgProduits'>";
-            	$img = Gamme::getAllFromID($p->getId(), Volume::createFromID($test[$i])->getId())->getImage();
-            	$vol = Volume::createFromID($test[$i])->getNom();
-            	$rep.= "<a><img src='img/{$img}'></a><br>";
-            	$rep .= "<a>{$vol}</a><br>";
-            	$rep .= "</div>";
-            }            
-		}
+	    $request =<<<SQL
+SELECT * FROM produit ORDER BY idProduit
+SQL;
+		$produit = myPDO::getInstance()->prepare($request);
+        $produit->setFetchMode(PDO::FETCH_CLASS, __CLASS__);
+        $produit->execute([]);
 
-        $rep .= "</div>";
-		return $rep;
+        if ($produit)
+            return $produit->fetchAll();
+        else
+            throw new Exception('Erreur, aucun produit trouvé');
 	}
 
 }
